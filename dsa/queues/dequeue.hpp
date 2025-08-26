f71511a04d0ad189fc21b965b35b6989790f3179
Ani <aniruthkarthik10@gmail.com>
2025-08-26 23:36:04 +0530

    added dequeue for todays part

--- dsa/queues/dequeue.hpp
+++ dsa/queues/dequeue.hpp
@@ -1,14 +1,6 @@
 #include <iostream>
 using namespace std;
 
-struct Node
-{
-	int val;
-	Node *prev;
-	Node *next;
-	Node(int x) : val(x), prev(nullptr), next(nullptr) {}
-};
-
 class Dequeue {
   private:
 	int capacity;
@@ -18,20 +10,23 @@ class Dequeue {
 	int count;
 
   public:
-	Dequeue(int x)
-	    : capacity(x), front(0), rear(0), count(0), arr(new int[capacity]) {};
+	Dequeue(int x) : capacity(x), front(-1), rear(-1), count(0), arr(new int[x])
+	{
+	}
+
+	~Dequeue() { delete[] arr; }
 
 	bool is_empty() { return count == 0; }
-
 	bool is_full() { return count == capacity; }
 
 	void push_front(int val)
 	{
-		if (is_empty())
+		if (is_full())
 		{
-			cout << "Dequeue underflow" << endl;
+			cout << "Dequeue overflow\n";
+			return;
 		}
-		if (front == -1)
+		if (is_empty())
 		{
 			front = rear = 0;
 		}
@@ -41,5 +36,96 @@ class Dequeue {
 		}
 		arr[front] = val;
 		count++;
+	}
+
+	void push_back(int val)
+	{
+		if (is_full())
+		{
+			cout << "Dequeue overflow\n";
+			return;
+		}
+		if (is_empty())
+		{
+			front = rear = 0;
+		}
+		else
+		{
+			rear = (rear + 1) % capacity;
+		}
+		arr[rear] = val;
+		count++;
+	}
+
+	void pop_front()
+	{
+		if (is_empty())
+		{
+			cout << "Dequeue underflow\n";
+			return;
+		}
+		if (front == rear)
+		{ // only one element
+			front = rear = -1;
+		}
+		else
+		{
+			front = (front + 1) % capacity;
+		}
+		count--;
+	}
+
+	void pop_back()
+	{
+		if (is_empty())
+		{
+			cout << "Dequeue underflow\n";
+			return;
+		}
+		if (front == rear)
+		{ // only one element
+			front = rear = -1;
+		}
+		else
+		{
+			rear = (rear - 1 + capacity) % capacity;
+		}
+		count--;
+	}
+
+	int get_front()
+	{
+		if (is_empty())
+		{
+			cout << "Deque empty\n";
+			return -1;
+		}
+		return arr[front];
+	}
+
+	int get_back()
+	{
+		if (is_empty())
+		{
+			cout << "Deque empty\n";
+			return -1;
+		}
+		return arr[rear];
+	}
+
+	void display()
+	{
+		if (is_empty())
+		{
+			cout << "Deque empty\n";
+			return;
+		}
+		int i = front;
+		for (int c = 0; c < count; c++)
+		{
+			cout << arr[i] << " ";
+			i = (i + 1) % capacity;
+		}
+		cout << endl;
 	}
 };
